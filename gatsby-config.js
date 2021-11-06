640bde03e5eca277e3f697d0bad883668fa4f4c6
Adilson Bacelar <a.m.bacelar@gmail.com>
2021-11-06 14:06:00 +0000

    imported firestor data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,40 @@
+require('dotenv').config();
+
+const firestoreCredentials = JSON.parse(process.env.FIRESTORE_CREDENTIALS);
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "kimbundu-gatsby",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'kimbundu gatsby',
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
+      },
+    },
+    {
+      resolve: 'gatsby-source-firestore',
+      options: {
+        credential: firestoreCredentials,
+        types: [
+          {
+            type: 'Entry',
+            collection: 'dictionary',
+            map: (doc) => ({
+              class: doc.class,
+              context: doc.context,
+              diacritricFree: doc.diacritricFree,
+              kimbunduText: doc.kimbunduText,
+              literalTranslations: doc.literalTranslations,
+              portugueseText: doc.portugueseText,
+              tags: doc.tags,
+            }),
+          },
+        ],
       },
     },
   ],
